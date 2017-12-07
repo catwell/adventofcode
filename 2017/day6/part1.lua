f0a65e4b91598e7e46eb3687144423f22cfa030d
Pierre Chapuis <catwell@archlinux.us>
2017-12-07 07:46:21 +0100

    day6

---
+++ 2017/day6/part1.lua
@@ -0,0 +1,37 @@
+local util = require "util"
+
+local function update(t)
+    local l, p = #t, 1
+    for i = 2, l do
+        if t[i] > t[p] then p = i end
+    end
+    local c = t[p]
+    t[p] = 0
+    for i = 1, l do
+        t[i] = t[i] + (c // l)
+    end
+    for i = p + 1, p + (c % l) do
+        local j = i % l
+        if j == 0 then j = l end
+        t[j] = t[j] + 1
+    end
+end
+
+
+local t, s = {}, util.read_file(arg[1])
+
+local function f(x) table.insert(t, tonumber(x)) end
+s:gsub("%d+", f)
+
+local c, seen = 0, {}
+
+while true do
+    s = table.concat(t, " ")
+    if seen[s] then
+        print(c)
+        return
+    end
+    update(t)
+    c = c + 1
+    seen[s] = true
+end
