fe25d5672c2e0583c2e587bb78cf3136dd85e618
Pierre Chapuis <catwell@archlinux.us>
2017-12-08 23:31:37 +0100

    day8

---
+++ 2017/day8/part2.lua
@@ -0,0 +1,33 @@
+local R = {}
+local m = -math.huge
+
+local ops = {
+    [">"] = function(a, b) return a > b end,
+    ["<"] = function(a, b) return a < b end,
+    [">="] = function(a, b) return a >= b end,
+    ["<="] = function(a, b) return a <= b end,
+    ["=="] = function(a, b) return a == b end,
+    ["!="] = function(a, b) return a ~= b end,
+}
+
+local function process(l)
+    local r1, i, n1, r2, c, n2 = l:match(
+        "(%w+) (%w+) ([0-9-]+) if (%w+) ([<>=!]+) ([0-9-]+)"
+    )
+
+    n1 = assert(tonumber(n1))
+    n2 = assert(tonumber(n2))
+
+    if ops[c](R[r2] or 0, n2) then
+        if i == "inc" then
+            R[r1] = (R[r1] or 0) + n1
+        else
+            assert(i == "dec")
+            R[r1] = (R[r1] or 0) - n1
+        end
+        if R[r1] > m then m = R[r1] end
+    end
+end
+
+for l in io.lines(arg[1]) do process(l) end
+print(m)
