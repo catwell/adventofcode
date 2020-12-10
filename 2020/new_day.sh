ad0b1e833e67928b2db73453f09d2fc55e4f3e45
Pierre Chapuis <catwell@archlinux.us>
2020-12-10 22:41:28 +0100

    2020 day09

--- 2020/new_day.sh
+++ 2020/new_day.sh
@@ -3,7 +3,7 @@
 last_day_dir () { find . -type d -name 'day[012]*' | sort | tail -n1 ; }
 
 new_day_dir () {
-    local n="$(last_day_dir | tail -c3)"
+    local n="$(last_day_dir | tail -c3 | sed 's/^0*//')"
     printf "day%02d" "$(($n+1))"
 }
 
