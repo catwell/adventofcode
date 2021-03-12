21c7c3df16deb17f7b9b5f0f44f548b4bc051b34
Pierre Chapuis <catwell@archlinux.us>
2021-03-12 21:34:51 +0100

    update tl.sh to support latest Teal master + add run_all.sh

--- 2020/tl.sh
+++ 2020/tl.sh
@@ -5,6 +5,9 @@ die () { >&2 echo "$@"; exit 1 ; }
 [ -z "$TL_BIN_PATH" ] && die "You must set $TL_BIN_PATH."
 
 path="$(dirname "$0")"
+lua="$path/.lua/bin/lua"
 
-"$path"/.lua/bin/lua "$TL_BIN_PATH" \
+lua_path="$("$lua" -e 'print(package.path)')"
+
+LUA_PATH="$(dirname $TL_BIN_PATH)/?.lua;$lua_path" "$lua" "$TL_BIN_PATH" \
     --include-dir "$path" --skip-compat53 "$@"
